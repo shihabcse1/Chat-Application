0f8b2bacd3a6c7dcb641913ea6bc14f827bd8926
shihabcse1 <tomdshihab@gmail.com>
2022-11-28 20:05:14 +0600

    search user from database added

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -2,7 +2,11 @@
 const express = require("express");
 
 // internal imports
-const { getInbox } = require("../controller/inboxController");
+const {
+    getInbox,
+    searchUser,
+    addConversation,
+} = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const { checkLogin } = require("../middlewares/common/checkLogin");
 
@@ -10,5 +14,11 @@ const router = express.Router();
 
 // inbox page
 router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
 
 module.exports = router;

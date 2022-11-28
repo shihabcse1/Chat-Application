128c08b39a32f6e2eddda6c05f2caf5de8159c4f
shihabcse1 <tomdshihab@gmail.com>
2022-11-28 23:35:15 +0600

    file attacted, send message added

--- controller/inboxController.js
+++ controller/inboxController.js
@@ -3,6 +3,7 @@ const createError = require("http-errors");
 // internal imports
 const User = require("../models/People");
 const Conversation = require("../models/Conversation");
+const Message = require("../models/Message");
 const escape = require("../utilities/escape");
 
 // get inbox page
@@ -95,8 +96,110 @@ async function addConversation(req, res, next) {
     }
 }
 
+// get messages of a conversation
+async function getMessages(req, res, next) {
+    try {
+        const messages = await Message.find({
+            conversation_id: req.params.conversation_id,
+        }).sort("-createdAt");
+
+        const { participant } = await Conversation.findById(
+            req.params.conversation_id
+        );
+
+        res.status(200).json({
+            data: {
+                messages: messages,
+                participant,
+            },
+            user: req.user.userid,
+            conversation_id: req.params.conversation_id,
+        });
+    } catch (err) {
+        res.status(500).json({
+            errors: {
+                common: {
+                    msg: "Unknows error occured!",
+                },
+            },
+        });
+    }
+}
+
+// send new message
+async function sendMessage(req, res, next) {
+    if (req.body.message || (req.files && req.files.length > 0)) {
+        try {
+            // save message text/attachment in database
+            let attachments = null;
+
+            if (req.files && req.files.length > 0) {
+                attachments = [];
+
+                req.files.forEach((file) => {
+                    attachments.push(file.filename);
+                });
+            }
+
+            const newMessage = new Message({
+                text: req.body.message,
+                attachment: attachments,
+                sender: {
+                    id: req.user.userid,
+                    name: req.user.username,
+                    avatar: req.user.avatar || null,
+                },
+                receiver: {
+                    id: req.body.receiverId,
+                    name: req.body.receiverName,
+                    avatar: req.body.avatar || null,
+                },
+                conversation_id: req.body.conversationId,
+            });
+
+            const result = await newMessage.save();
+
+            // emit socket event
+            global.io.emit("new_message", {
+                message: {
+                    conversation_id: req.body.conversationId,
+                    sender: {
+                        id: req.user.userid,
+                        name: req.user.username,
+                        avatar: req.user.avatar || null,
+                    },
+                    message: req.body.message,
+                    attachment: attachments,
+                    date_time: result.date_time,
+                },
+            });
+
+            res.status(200).json({
+                message: "Successful!",
+                data: result,
+            });
+        } catch (err) {
+            res.status(500).json({
+                errors: {
+                    common: {
+                        msg: err.message,
+                    },
+                },
+            });
+        }
+    } else {
+        res.status(500).json({
+            errors: {
+                common: "message text or attachment is required!",
+            },
+        });
+    }
+}
+
 module.exports = {
     getInbox,
     searchUser,
     addConversation,
+    getMessages,
+    sendMessage,
 };
--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -6,9 +6,12 @@ const {
     getInbox,
     searchUser,
     addConversation,
+    getMessages,
+    sendMessage,
 } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const { checkLogin } = require("../middlewares/common/checkLogin");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 
 const router = express.Router();
 
@@ -20,5 +23,11 @@ router.post("/search", checkLogin, searchUser);
 
 // add conversation
 router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
 
 module.exports = router;
